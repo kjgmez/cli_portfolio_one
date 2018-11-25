41e59c9b56319971e27aa35a35399aa5f0a5aa72
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-25 23:03:27 +0000

    trouble shoot time

--- cli_players_on_redbull/lib/cli.rb
+++ cli_players_on_redbull/lib/cli.rb
@@ -1,12 +1,14 @@
-require_relative '../config/environment.rb'
-
-def call
+#require_relative '../config/environment.rb'
+require_relative './player'
+require_relative './scraper'
+class Cli
+  def call
     input = ""
     message = ["Welcome to the Redbull Team line up!",
-      "To list all of the players by name, enter 'list by name'.",
-      "To list all of the players by position, enter 'list by position'.",
-      "To quit, type 'exit'.",
-      "What would you like to do?"]
+        "To list all of the players by name, enter 'list by name'.",
+        "To list all of the players by position, enter 'list by position'.",
+        "To quit, type 'exit'.",
+        "What would you like to do?"]
     message.each{|phrase| puts phrase}
     while input != "exit"
       input = gets
@@ -21,6 +23,10 @@ def call
   end
 
   def list_name
+    Player.all.each_with_index{|player, i| puts "#{i}. #{player.name}, plays #{player.position}"}
   end
   def list_position
   end
+end
+
+Cli.new.call
