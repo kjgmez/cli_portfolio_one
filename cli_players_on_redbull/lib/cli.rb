af36d01467446c408e4cb3490a9e5e6b6788b392
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-26 00:39:15 +0000

    debugging cli

--- cli_players_on_redbull/lib/cli.rb
+++ cli_players_on_redbull/lib/cli.rb
@@ -5,29 +5,22 @@ require 'pry'
 class Cli
   def call
     message = ["Welcome to the Redbull Team line up!",
-        "To list all of the players by name, enter 'list by name'.",
-        "To list all of the players by position, enter 'list by position'.",
+        "Type 'list' to see all the players and their positions",
         "To quit, type 'exit'.",
         "What would you like to do?"]
     message.each{|phrase| puts phrase}
     input = gets
-    while input != "exit"
-      if input == "list by name"
-        list_name
-      elsif input == "list by position"
-        list_position
-      else
-        nil
-      end
+    case input
+    when "list"
+      list_name
+    when "exit"
+      puts 'Exiting'
     end
   end
 
   def list_name
     Player.all.each_with_index{|player, i| puts "#{i+1}. #{player.name}, plays #{player.position}"}
-    #binding.pry
-  end
-  def list_position
   end
 end
 
-Cli.new.list_name
+Cli.new.call
